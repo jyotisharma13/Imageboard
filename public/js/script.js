74335334ec41ba564dedd608316242d1e350f5a5
jyoti <sharma.jyoti27april@gmail.com>
2019-01-22 10:24:12 +0100

    imageboard part-1

--- public/js/script.js
+++ public/js/script.js
@@ -6,9 +6,11 @@
             images: []
         },
         mounted: function() {
+            var self= this;
+            // then runs when we get the respose fron the server
             axios.get('/images').then(function(response) {
                 console.log("response form the images: ", response);
-                this.images = response.data;
+                self.images = response.data;
             });
         }
     });
